59f2d70706b2f8ae4e5bca7c36f6777e7057a326
Alvaroguay123 <126633750+cabezadepingo123@users.noreply.github.com>
2023-02-28 23:53:52 -0300

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,8 @@
+document.getElementById('boton').addEventListener('click', function(){
+    console.log("Hola que tal me llamo Sanchez Tomas y esta es mi informacion");
+    document.getElementById("demo").innerHTML = "Soy una persona proactiva, organizada y responsable, con buenas relaciones interpersonales.Siempre tengo las mejores disposición para la realizacion de mis labores.Busco un puesto de trabajo desafiante."
+});
+document.getElementById('boton_idioma').addEventListener('click', function(){
+    console.log("Estos son algunos de los idiomas que conozco y aprendi");
+    document.getElementById("demo").innerHTML = "Idiomas: Español - Ingles - Frances Basico - Italiano Basico."
+});
